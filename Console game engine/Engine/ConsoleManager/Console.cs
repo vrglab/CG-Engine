54cf432328dd256c0d88c437efcd000aeff45764
Vrglab <vrglab1@gmail.com>
2023-02-16 12:23:04 +0100

    Addes more fuctionality to the Console class

--- Console game engine/Engine/ConsoleManager/Console.cs
+++ Console game engine/Engine/ConsoleManager/Console.cs
@@ -13,6 +13,8 @@ namespace Engine.ConsoleManager
         {
             return (System.Console.KeyAvailable && System.Console.ReadKey(true).Key == key);
         } 
+
+        //UTF-16 based char writers
         
         public static void Write(int code)
         {
@@ -27,7 +29,6 @@ namespace Engine.ConsoleManager
             System.Console.Write(charcater);
             System.Console.ResetColor();
         }
-
 
         public static void Write(int code, Vector2 writAt)
         {
@@ -100,6 +101,85 @@ namespace Engine.ConsoleManager
         }
 
         public static void Replace(int Char, Vector2 RemovecharAt, Vector2 addCharAt)
+        {
+            Remove(RemovecharAt);
+            WriteLine(Char, addCharAt);
+        }
+
+
+        //Full text writers
+
+        public static void Write(string code)
+        {
+            System.Console.Write(code);
+        }
+
+        public static void Write(string code, ConsoleColor color)
+        {
+            System.Console.ForegroundColor = color;
+            System.Console.Write(code);
+            System.Console.ResetColor();
+        }
+
+        public static void Write(string code, Vector2 writAt)
+        {
+            System.Console.SetCursorPosition(writAt.x, writAt.y);
+            System.Console.Write(code);
+        }
+
+        public static void Write(string code, ConsoleColor color, Vector2 writAt)
+        {
+            System.Console.SetCursorPosition(writAt.x, writAt.y);
+            System.Console.ForegroundColor = color;
+            System.Console.Write(code);
+            System.Console.ResetColor();
+        }
+
+        public static void WriteLine(string code)
+        {
+            System.Console.WriteLine(code);
+        }
+
+        public static void WriteLine(string code, ConsoleColor color)
+        {
+            System.Console.ForegroundColor = color;
+            System.Console.WriteLine(code);
+            System.Console.ResetColor();
+        }
+
+        public static void WriteLine(string code, Vector2 writAt)
+        {
+            System.Console.SetCursorPosition(writAt.x, writAt.y);
+            System.Console.WriteLine(code);
+        }
+
+        public static void WriteLine(string code, ConsoleColor color, Vector2 writAt)
+        {
+            System.Console.SetCursorPosition(writAt.x, writAt.y);
+            System.Console.ForegroundColor = color;
+            System.Console.WriteLine(code);
+            System.Console.ResetColor();
+        }
+
+        public static void Replace(string Char, Vector2 charAt)
+        {
+            Remove(charAt);
+            WriteLine(Char, charAt);
+        }
+
+        public static void Replace(string Char, ConsoleColor color, Vector2 charAt)
+        {
+            Remove(charAt);
+            WriteLine(Char, color, charAt);
+        }
+
+        public static void Replace(string Char, ConsoleColor color, Vector2 RemovecharAt, Vector2 addCharAt)
+        {
+            Remove(RemovecharAt);
+            WriteLine(Char, color, addCharAt);
+        }
+
+        public static void Replace(string Char, Vector2 RemovecharAt, Vector2 addCharAt)
         {
             Remove(RemovecharAt);
             WriteLine(Char, addCharAt);
