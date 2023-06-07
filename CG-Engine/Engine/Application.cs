b40159b1a15d901a80e7565b7a21c38b6054c970
vrglab <vrglab1@gmail.com>
2023-06-07 15:17:13 +0200

    sdf

--- CG-Engine/Engine/Application.cs
+++ CG-Engine/Engine/Application.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Engine.ConsoleManager;
-using Engine.ConsoleManager.Layering;
 using Engine.LevelManagement;
 using Engine.Renderer;
 using Console = Engine.ConsoleManager.Console;
--- CG-Engine/Engine/Object/PremadeComponents/CharacterRenderer.cs
+++ CG-Engine/Engine/Object/PremadeComponents/CharacterRenderer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Engine.ConsoleManager.Layering;
 using Engine.Mathmatics;
 using Engine.Renderer;
 using Console = Engine.ConsoleManager.Console;
