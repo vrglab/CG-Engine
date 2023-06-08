7d63870314bc535f98e15f5bc795bf4cf6bd132a
vrglab <vrglab1@gmail.com>
2023-06-08 14:10:20 +0200

    idek

--- CG-Engine/Engine/ConsoleManager/Console.cs
+++ CG-Engine/Engine/ConsoleManager/Console.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using Engine.ConsoleManager.Layering;
 using Engine.Mathmatics;
 
 namespace Engine.ConsoleManager
